9547f644b4f32081907601f6a50bc78aa9503fe3
Ahmed khan <ahmed_khan019@hotmail.com>
2023-10-25 12:49:59 +0500

    Added react select dropdown for palettes selection

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { SimpleBarChart } from '@carbon/charts-react';
 import data from './data.js';
 import options from './options.js';
 import '@carbon/charts-react/styles.css';
+import Select from 'react-select';
 
 class App extends React.Component {
   state = {
@@ -12,9 +13,31 @@ class App extends React.Component {
     selectedColor: { value: 1, label: 'Color 1' },
   };
 
+  handleColorChange = (selectedOption) => {
+    console.log(selectedOption.value);
+    this.setState({
+      options: {
+        color: {
+          pairing: {
+            option: selectedOption.value,
+          },
+        },
+      },
+      selectedColor: selectedOption,
+    });
+  };
+
   render() {
     return (
       <div>
+        <Select
+          value={this.state.selectedColor}
+          onChange={this.handleColorChange}
+          options={[
+            { value: 1, label: 'Color 1' },
+            { value: 2, label: 'Color 2' },
+          ]}
+        />
         <SimpleBarChart data={this.state.data} options={this.state.options} />
       </div>
     );
